0f30d1b6b58bfcee2a03b7939a6d4d855871e44a
Velosoda <velosoda@kean.edu>
2016-09-16 13:22:02 -0400

    Add files via upload

---
+++ src/com/manager/MainAppMain/Market.java
@@ -0,0 +1,14 @@
+package com.manager.MainAppMain;
+
+import java.util.ArrayList;
+
+
+public class Market
+{
+	 ArrayList<Player> marketGoalie = new ArrayList<Player>();
+	 ArrayList<Player> marketDefenders = new ArrayList<Player>();
+	 ArrayList<Player> marketMidFielders = new ArrayList<Player>();
+	 ArrayList<Player> marketAttackers = new ArrayList<Player>();
+	 ArrayList<Player> marketYouth= new ArrayList<Player>();
+	 
+}
