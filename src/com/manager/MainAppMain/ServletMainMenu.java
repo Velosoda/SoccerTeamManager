a6b3b1f70394458260d40571d569268ac9e95150
Velosoda <velosoda@kean.edu>
2017-02-01 15:29:16 -0500

    Update ServletMainMenu.java

--- src/com/manager/MainAppMain/ServletMainMenu.java
+++ src/com/manager/MainAppMain/ServletMainMenu.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/MainMenu")
+
+
 public class ServletMainMenu extends HttpServlet {
 	
     private static final long serialVersionUID = 1L;
