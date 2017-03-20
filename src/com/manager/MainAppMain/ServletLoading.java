0f8f2c617c047494e1a443a93b0ede4f13edc4ec
cabiler <mjdilone7794@gmail.com>
2017-03-20 18:28:11 -0400

    Add files via upload
    Michael - 3/20

--- src/com/manager/MainAppMain/ServletLoading.java
+++ src/com/manager/MainAppMain/ServletLoading.java
@@ -1,18 +1,34 @@
 package com.manager.MainAppMain;
 
-import java.io.IOException;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.*;
 
 @WebServlet("/Loading")
 public class ServletLoading extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	//namesList has to be made here. This was ultimatly the only way to create the array using a file since the path to names doesnt exist outside of this instance of the servlet. Don't quote me on that 
+	public ArrayList<String> makeNames() throws IOException
+	{
+		ArrayList<String> namesList = new ArrayList<String>();
+		BufferedReader br = new BufferedReader(new FileReader(getServletContext().getRealPath("/names.txt")));
+		String line = br.readLine();
+		while (line != null) 
+		{ 
+			namesList.add(line); line = br.readLine(); 
+			}
+		br.close();
+		return namesList;
+	
+	}
+	
+	
     public ServletLoading() 
     {
         super();      
@@ -25,8 +41,20 @@ public class ServletLoading extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
+		
+			ArrayList<String> namesList = new ArrayList<String>();
+			BufferedReader br = new BufferedReader(new FileReader(getServletContext().getRealPath("/names.txt")));
+			String line = br.readLine();
+			while (line != null) 
+			{ 
+				namesList.add(line); line = br.readLine(); 
+				}
+			br.close();
+		
+		
 		ServletContext STM = getServletConfig().getServletContext();
-		Market market = new Market(); 
+		//the market takes in an ArrayList<String> so that it can then eventually pass it on to the player constructor
+		Market market = new Market(makeNames());
 		League league = new League(market); 
 		ModelLoading ml = new ModelLoading();
 		ml.fillTotalMarket(market.marketAttacker);
@@ -37,6 +65,7 @@ public class ServletLoading extends HttpServlet {
 		STM.setAttribute("market", market);
 		STM.setAttribute("league", league);
 		STM.setAttribute("ModelLoading", ml);
+
 		request.getRequestDispatcher("/JSP/mainmenu.jsp").forward(request, response);
 	}
 }
