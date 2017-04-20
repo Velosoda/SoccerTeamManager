6bfcc775edb4e367b8f853b4cebf7e584ef3c63d
Velosoda <velosoda@kean.edu>
2017-04-20 11:42:38 -0400

    Main menu Passes TeamStarters to the view

--- src/com/manager/MainAppMain/ServletMainMenu.java
+++ src/com/manager/MainAppMain/ServletMainMenu.java
@@ -1,6 +1,9 @@
 package com.manager.MainAppMain;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,6 +23,20 @@ public class ServletMainMenu extends HttpServlet {
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
+		ServletContext STM = getServletConfig().getServletContext();
+		Market market = (Market)STM.getAttribute("market");
+		League league = (League)STM.getAttribute("league");
+		ModelLoading ml = (ModelLoading)STM.getAttribute("ModelLoading");
+		
+		ArrayList<Player> teamStarters = league.allTeams.get(Constants.userTeamId).teamStarters;
+		ArrayList<Player> teamBench = league.allTeams.get(Constants.userTeamId).teamBench;
+		
+		STM.setAttribute("market", market);
+		STM.setAttribute("league", league); 
+		STM.setAttribute("ModelLoading", ml);
+		
+		request.setAttribute("teamStarters", teamStarters);
+		request.setAttribute("teamBench", teamBench);
 		request.getRequestDispatcher("/JSP/mainmenu.jsp").forward(request, response);
 	}
 }
